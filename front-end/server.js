bc273c5cbc9ace831b268a4f838fbea704277b76
Jack <jack@Jacks-MacBook-Air.local>
2019-02-20 16:00:49 -0500

    https

--- front-end/server.js
+++ front-end/server.js
@@ -1,7 +1,21 @@
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 
 const express = require('express');
 const path = require('path');
 const app = express();
+
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
 
 
 app.use(express.static(path.join(__dirname, 'build')));
@@ -10,4 +24,14 @@ app.get('/*', function (req, res) {
    res.sendFile(path.join(__dirname, 'build', 'index.html'));
  });
 
-app.listen(80);
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
